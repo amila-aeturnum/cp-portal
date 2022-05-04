19bd4bb692272fe0c58639dd9c027af0007ce9ef
manojasela1 <manojasela@outlook.com>
2022-05-04 23:04:37 +0530

    add msal config

---
+++ src/configs/azureConfig.ts
@@ -0,0 +1,37 @@
+const tenantName = 'sachithatestapp2';
+const signInPolicy = 'B2C_1A_DEMO_SIGNUP_SIGNIN_FORCEPASSWORDRESET';
+const AuthorityUrl = `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${signInPolicy}`;
+const msalConfig = {
+	auth: {
+		clientId: 'afcff42d-55cc-45ea-8475-58214904c683', //"e3197c57-b74b-4d2c-98e7-7607f22e91f2",
+		authority: AuthorityUrl, //"https://ConversionPathTest.b2clogin.com/ConversionPathTest.onmicrosoft.com/B2C_1_cpsignupandsignin", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+		redirectUri: 'http://localhost:3000',
+		knownAuthorities: [`${tenantName}.b2clogin.com`],
+		validateAuthority: false,
+		postLogoutRedirectUri: 'http://localhost:3000',
+		navigateToLoginRequestUrl: true,
+		client_secret: 'nEo8Q~XyvtT3xjEbGTuCMvocme2bRoJVkMSF.dcm', //"ETb8Q~~WwPfzRa0Lc5-T~w35Ys3JPTqI0EfoVbbc"
+		grant_type: 'authorization_code',
+		grantType: 'authorization_code'
+	},
+	cache: {
+		cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+		storeAuthStateInCookie: false // Set this to "true" if you are having issues on IE11 or Edge
+	}
+};
+
+// Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+// const loginRequest = {
+//    scopes: ["e3197c57-b74b-4d2c-98e7-7607f22e91f2"]
+//   };
+const loginRequest = {
+	scopes: ['afcff42d-55cc-45ea-8475-58214904c683 openid offline_access']
+};
+
+// Add the endpoints here for Microsoft Graph API services you'd like to use.
+const graphConfig = {
+	graphMeEndpoint: 'https://graph.microsoft.com/oidc/userinfo'
+};
+//graph.windows.net
+
+export { msalConfig, loginRequest, graphConfig };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,9 @@ import { theme } from 'configs/theme';
 import { Router } from 'next/router';
 import { useState } from 'react';
 import Loader from 'components/atoms/Loader';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from 'configs/azureConfig';
+import { PublicClientApplication } from '@azure/msal-browser';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [loading, setLoading] = useState(false);
@@ -13,10 +16,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 	Router.events.on('routeChangeStart', () => setLoading(true));
 	Router.events.on('routeChangeComplete', () => setLoading(false));
 
+	const msalInstance = new PublicClientApplication(msalConfig);
+
 	return (
-		<ThemeProvider theme={theme}>
-			<Layout>{!loading ? <Component {...pageProps} /> : <Loader />}</Layout>
-		</ThemeProvider>
+		<MsalProvider instance={msalInstance}>
+			<ThemeProvider theme={theme}>
+				<Layout>{!loading ? <Component {...pageProps} /> : <Loader />}</Layout>
+			</ThemeProvider>
+		</MsalProvider>
 	);
 }
 
