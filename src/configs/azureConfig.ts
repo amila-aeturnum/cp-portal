a429e7ff44bca782e71c2f6e484df4d78d9267e7
sachitha-h-aet <90398327+sachitha-h-aet@users.noreply.github.com>
2022-05-09 13:13:00 +0530

    update dev configs

--- src/configs/azureConfig.ts
+++ src/configs/azureConfig.ts
@@ -1,20 +1,17 @@
-const tenantName = 'sachithatestapp2';
-const signInPolicy = 'B2C_1A_DEMO_SIGNUP_SIGNIN_FORCEPASSWORDRESET';
+const tenantName = 'devconversionpath';
+const signInPolicy = 'B2C_1A_SIGNUP_SIGNIN_FORCEPASSWORDRESET';
 const AuthorityUrl = `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${signInPolicy}`;
 const PasswordResetAuthorityUrl = `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/B2C_1A_PASSWORDRESET`;
+const clientID = '0527feb0-5ebb-4096-8c5d-0ffeb4df4b8a';
+const scope = `${clientID} openid offline_access`;
 const msalConfig = {
 	auth: {
-		clientId: 'afcff42d-55cc-45ea-8475-58214904c683', //"e3197c57-b74b-4d2c-98e7-7607f22e91f2",
-		authority: AuthorityUrl, //"https://ConversionPathTest.b2clogin.com/ConversionPathTest.onmicrosoft.com/B2C_1_cpsignupandsignin", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+		clientId: clientID,
+		authority: AuthorityUrl,  // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
 		redirectUri: typeof window !== 'undefined' ? window.location.origin : '',
 		knownAuthorities: [`${tenantName}.b2clogin.com`],
 		validateAuthority: false,
 		postLogoutRedirectUri: typeof window !== 'undefined' ? window.location.origin : '',
-		//navigateToLoginRequestUrl: true,
-		// client_secret: 'nEo8Q~XyvtT3xjEbGTuCMvocme2bRoJVkMSF.dcm', //"ETb8Q~~WwPfzRa0Lc5-T~w35Ys3JPTqI0EfoVbbc"
-		// grant_type: 'authorization_code',
-		// grantType: 'authorization_code',
-		resetPasswordPolicy: 'B2C_1A_PASSWORDRESET',
 		navigateToLoginRequestUrl: false
 	},
 	cache: {
@@ -24,14 +21,14 @@ const msalConfig = {
 };
 
 const forgotPasswordRequest = {
-	scopes: ['afcff42d-55cc-45ea-8475-58214904c683 openid offline_access'],
+	scopes: [scope],
 	authority: PasswordResetAuthorityUrl,
 	navigateToLoginRequestUrl: false
 };
 
 // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
 const loginRequest = {
-	scopes: ['afcff42d-55cc-45ea-8475-58214904c683 openid offline_access']
+	scopes: [scope]
 };
 
 // Add the endpoints here for Microsoft Graph API services you'd like to use.
