d0e465ad45053d3c98c527106c024e07c7e1e92e
sangeeth-22 <sangeeth.p@aeturnum.com>
2022-05-11 16:17:41 +0530

    changes

--- src/pages/user-management/clients.tsx
+++ src/pages/user-management/clients.tsx
@@ -1,17 +1,101 @@
 import type { NextPage } from 'next';
 import styles from '../../../styles/Home.module.css';
 import * as React from 'react';
-import { DataGrid, GridColumns, GridRowsProp } from '@mui/x-data-grid';
-import { randomCreatedDate, randomTraderName, randomUpdatedDate } from '@mui/x-data-grid-generator';
 import Breadcrumb from 'components/molecules/Breadcrumb';
+import ResponsiveDialog from 'components/molecules/ResponsiveDialog';
+import { Button, Grid, OutlinedInput, InputAdornment } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import AddIcon from '@mui/icons-material/Add';
+import DataTable from 'components/molecules/DataTable';
+import CPButton from 'components/atoms/CPButton';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Clients: NextPage = () => {
+	const [open, setOpen] = React.useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleEdit = (data: object) => {
+		alert(JSON.stringify(data));
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const handleDataExport = () => {
+		alert('not implemented');
+	};
+
+	const dialogContent = (
+		<Grid container spacing={3} sx={{ marginTop: '10px' }}>
+			<Grid item xs={6}>
+				<TextField id="outlined-basic" label="Outlined" variant="outlined" fullWidth />
+			</Grid>
+			<Grid item xs={6}>
+				<TextField id="filled-basic" label="Filled" variant="outlined" fullWidth />
+			</Grid>
+			<Grid item xs={6}>
+				<TextField id="filled-basic" label="Filled" variant="outlined" fullWidth />
+			</Grid>
+			<Grid item xs={6}>
+				<TextField id="filled-basic" label="Filled" variant="outlined" fullWidth />
+			</Grid>
+		</Grid>
+	);
+
 	return (
 		<div className={styles.container}>
-			<Breadcrumb title={'Clients'} />
+			<Breadcrumb
+				title={'Clients'}
+				actions={<CPButton label={'Add a Client'} startIcon={<AddIcon />} onClick={handleOpen} variant="contained" />}
+			/>
 			<main className={styles.main}>
 				<div style={{ height: '60vh', width: '100%' }}>
-					{false && <DataGrid rows={rows} columns={columns} experimentalFeatures={{ newEditingApi: true }} />}
+					<ResponsiveDialog
+						title={'Create Client'}
+						content={dialogContent}
+						open={open}
+						handleClose={handleClose}
+						actions={
+							<>
+								<Button onClick={handleClose} variant="contained">
+									Close
+								</Button>
+							</>
+						}
+					/>
+					<Grid container sx={{ padding: '10px' }}>
+						<Grid item xs={4}>
+							<CPButton
+								label={'Export'}
+								startIcon={<FileUploadIcon />}
+								onClick={handleDataExport}
+								variant="contained"
+							/>
+						</Grid>
+						<Grid item xs={3}></Grid>
+						<Grid item xs={5} alignItems="right">
+							<OutlinedInput
+								id="standard-basic"
+								sx={{ border: '1px solid #A9A8A8', width: '100%' }}
+								startAdornment={
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								}
+								endAdornment={
+									<InputAdornment position="end">
+										<CPButton label={'Search'} onClick={handleDataExport} variant="contained" />
+									</InputAdornment>
+								}
+							/>
+						</Grid>
+					</Grid>
+
+					<DataTable onEditAction={handleEdit} />
 				</div>
 			</main>
 		</div>
@@ -19,60 +103,3 @@ const Clients: NextPage = () => {
 };
 
 export default Clients;
-
-const columns: GridColumns = [
-	{ field: 'name', headerName: 'Name', width: 180, editable: true },
-	{ field: 'age', headerName: 'Age', type: 'number', editable: true },
-	{
-		field: 'dateCreated',
-		headerName: 'Date Created',
-		type: 'date',
-		width: 180,
-		editable: true
-	},
-	{
-		field: 'lastLogin',
-		headerName: 'Last Login',
-		type: 'dateTime',
-		width: 220,
-		editable: true
-	}
-];
-
-const rows: GridRowsProp = [
-	{
-		id: 1,
-		name: randomTraderName(),
-		age: 25,
-		dateCreated: randomCreatedDate(),
-		lastLogin: randomUpdatedDate()
-	},
-	{
-		id: 2,
-		name: randomTraderName(),
-		age: 36,
-		dateCreated: randomCreatedDate(),
-		lastLogin: randomUpdatedDate()
-	},
-	{
-		id: 3,
-		name: randomTraderName(),
-		age: 19,
-		dateCreated: randomCreatedDate(),
-		lastLogin: randomUpdatedDate()
-	},
-	{
-		id: 4,
-		name: randomTraderName(),
-		age: 28,
-		dateCreated: randomCreatedDate(),
-		lastLogin: randomUpdatedDate()
-	},
-	{
-		id: 5,
-		name: randomTraderName(),
-		age: 23,
-		dateCreated: randomCreatedDate(),
-		lastLogin: randomUpdatedDate()
-	}
-];
