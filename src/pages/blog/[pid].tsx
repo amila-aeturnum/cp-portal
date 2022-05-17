21128781329a0c08dd6f7937ea8e7d2d5fee71ae
sangeeth-22 <85948334+sangeeth-22@users.noreply.github.com>
2022-05-17 14:37:39 +0530

    Update [pid].tsx

--- src/pages/blog/[pid].tsx
+++ src/pages/blog/[pid].tsx
@@ -1,11 +1,28 @@
 import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from 'next';
 
-const Post = () => {
+const Post = (props:any) => {
   const router = useRouter();
   const { pid } = router.query;
    const { sid } = router.query;
 
   return <p>Post: {pid} {sid}</p>;
 };
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const arr: string[] = ['1','2','3']
+    const paths = arr.map((pid) => {
+        return {
+            params: { pid },
+        }
+    })
+    return { paths }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    // This is where the error occurs
+    const { pid } = context.params // Property 'slug' does not exist on type 'ParsedUrlQuery | undefined'
+    return pid;
+}
 
 export default Post;
